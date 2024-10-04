a7159c35ac815ad6d75e29d170703c0a0f2c6b10
WeiGuo <58603231+guowei1314@users.noreply.github.com>
2024-10-05 00:36:37 +0800

    Update YaleA_run.m

--- YaleA_run.m
+++ YaleA_run.m
@@ -2,11 +2,11 @@ clear all;
 
 %rng(0);
 
-load("YaleA_0.1_Ubalaced_incomplete.mat");
+load("YaleA_0.2_Ubalaced_incomplete.mat");
 
 lambda1 =0.01 ;
-lambda2 = 1;
-order = 3;
+lambda2 = 0.1;
+order = 2;
 result = RMoGL(X,W,lambda1,lambda2,order,gt);
 fprintf("NMI = %5.4f，Purity = %5.4f，ARI = %5.4f\n",result(1),result(2),result(3));
 
