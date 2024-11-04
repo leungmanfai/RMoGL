60166033d7f53377d41c17e032f5996d73039cd2
WeiGuo <58603231+guowei1314@users.noreply.github.com>
2024-11-04 11:39:56 +0800

    Update YaleA_run.m

--- YaleA_run.m
+++ YaleA_run.m
@@ -6,7 +6,7 @@ load("YaleA_0.2_unbalanced_incomplete.mat");
 
 lambda1 =0.01 ;
 lambda2 = 0.1;
-order = 2;
+order = 4;
 result = RMoGL(X,W,lambda1,lambda2,order,gt);
 fprintf("NMI = %5.4f，Purity = %5.4f，ARI = %5.4f\n",result(1),result(2),result(3));
 
