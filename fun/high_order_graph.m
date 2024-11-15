b640e2e679b57ebb4fb6d466713cefcc125f8b26
WeiGuo <58603231+guowei1314@users.noreply.github.com>
2024-11-15 22:37:31 +0800

    Update high_order_graph.m

--- fun/high_order_graph.m
+++ fun/high_order_graph.m
@@ -4,7 +4,7 @@ N = size(x,1);
 if order ==1
     S = constructW_PKN(x',10);
     W2{1} = S;
-    W2{1} = W2{1} - diag(diag(W2{1}));
+    %W2{1} = W2{1} - diag(diag(W2{1}));
 else
    % S = 1 - pdist2(x, x, 'cosine');
     S = constructW_PKN(x',10);
@@ -13,7 +13,7 @@ else
 
     for o = 2:(order)
         W2{o} = W2{o-1}*W2{1};
-        W2{o} = W2{o} - diag(diag(W2{o}));
+        %W2{o} = W2{o} - diag(diag(W2{o}));
     end
 end
 
